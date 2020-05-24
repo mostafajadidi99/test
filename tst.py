cec200c570595df8387bb2456d6616ae109508ba
mostafajadidi99 <jadidi22@gmail.com>
2020-05-24 15:26:25 +0430

    test

---
+++ tst.py
@@ -0,0 +1,16 @@
+# a=50
+
+# if a>50:
+#     print('yes')
+# elif a<50:
+#     print("hemine")  
+# else:
+#     print('nadarimo')
+
+
+r = int(input('num'))
+
+if r%2==0:
+    print('{} is even' .format(r))
+else:
+    print('{} is fard',r)    
